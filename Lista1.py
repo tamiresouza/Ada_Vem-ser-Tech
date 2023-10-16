a0562c44128392ab5a43e44123b6eb0aa463a827
Tamires <tamires.crisouza@gmail.com>
2023-10-16 18:45:45 -0300

    Lista 1-adicinei o ex.1

---
+++ Lista1.py
@@ -0,0 +1,6 @@
+# 1. Faça um programa que peça a idade do usuário e imprima se ele é maior ou menor de 18 anos.
+idade = int(input("Digite sua idade"))
+if(idade)>=18:
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")
